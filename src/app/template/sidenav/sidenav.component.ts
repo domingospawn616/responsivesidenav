bb7096b363bb1dc417c12699807cd2ccec7fc32f
Domingos Souza <domingos.souza@tivit.com>
2024-02-23 13:32:11 -0300

    Animações

--- src/app/template/sidenav/sidenav.component.ts
+++ src/app/template/sidenav/sidenav.component.ts
@@ -4,13 +4,35 @@ import { NavData } from '../nav-data';
 import { RouterLinkActive, RouterModule } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { SideNavToggle } from './sidenavtoogle';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   imports: [RouterModule, MatIcon],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss'
+  styleUrl: './sidenav.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}), 
+        animate('1000ms', style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({opcacity: 1}),
+        animate('1000ms', style({opacity: 0}))
+      ])
+    ]),
+
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms', keyframes([
+          style({transform: 'rotate(0deg)', offset: '0'}),
+          style({transform: 'rotate(2turn)', offset: '1'}),
+        ]))
+      ]),
+    ]),
+  ],
 })
 export class SidenavComponent {
 
