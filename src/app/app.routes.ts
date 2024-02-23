3096dbab0ad131e87c9f203575a0484aa32bffdc
Domingos Souza <domingos.souza@tivit.com>
2024-02-23 11:00:07 -0300

    Rotas configuradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,19 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { CoupensComponent } from './pages/coupens/coupens.component';
+import { PagesComponent } from './pages/pages/pages.component';
+import { MediaComponent } from './pages/media/media.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
-    {path: 'dashboard', component: DashboardComponent}
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'products', component: ProductsComponent},
+    {path: 'statistics', component: StatisticsComponent},
+    {path: 'coupens', component: CoupensComponent},
+    {path: 'pages', component: PagesComponent},
+    {path: 'media', component: MediaComponent},
+    {path: 'settings', component: SettingsComponent},
 ];
--- src/app/template/nav-data-service.service.ts
+++ src/app/template/nav-data-service.service.ts
@@ -6,7 +6,13 @@ import { NavData } from './nav-data';
 })
 export class NavDataServiceService {
   protected navBars: NavData[] = [
-    { routeLink: 'dashboard', icon: 'home', label: 'Dashboard' }
+    { routeLink: 'dashboard', icon: 'home', label: 'Dashboard' },
+    { routeLink: 'products', icon: 'dataset', label: 'Products' },
+    { routeLink: 'statistics', icon: 'apps', label: 'Statistics' },
+    { routeLink: 'coupens', icon: 'create_new_folder', label: 'Coupens' },
+    { routeLink: 'pages', icon: 'content_copy', label: 'Pages' },
+    { routeLink: 'media', icon: 'play_circle', label: 'Media' },
+    { routeLink: 'settings', icon: 'settings', label: 'Settings' },
   ];
 
   constructor() { }
