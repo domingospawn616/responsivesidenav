16cda2733c32d452f9c73942a635431408075c42
Domingos Souza <domingos.souza@tivit.com>
2024-02-26 13:34:36 -0300

    Submenus com Scroll

--- src/app/template/nav-data-service.service.ts
+++ src/app/template/nav-data-service.service.ts
@@ -7,7 +7,51 @@ import { INavData } from './nav-data';
 export class NavDataServiceService {
   protected navBars: INavData[] = [
     { routeLink: 'dashboard', icon: 'home', label: 'Dashboard' },
-    { routeLink: 'products', icon: 'dataset', label: 'Products' },
+    {
+      routeLink: 'products', 
+      icon: 'dataset', 
+      label: 'Products',
+      items: [
+        {
+          routeLink: 'products/list', 
+          icon: '', 
+          label: 'Level 1.1',
+          items: [
+            {
+              routeLink: 'products/level1.1',
+              label: 'Level 1.1',
+              icon: '',
+              items: [
+                {
+                  routeLink: 'products/level2.1',
+                  label: 'Level 2.1',
+                  icon: ''
+                },{
+                  routeLink: 'products/level2.2',
+                  label: 'Level 2.2',
+                  icon: '',
+                  items: [
+                    {
+                      routeLink: 'products/level3.1',
+                      label: 'Level 3.1',
+                      icon: ''
+                    },{
+                      routeLink: 'products/level3.2',
+                      label: 'Level 3.2',
+                      icon: ''
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },{
+          routeLink: 'products/list', 
+          icon: '', 
+          label: 'Level 1.2',
+        }
+      ]
+    },
     { routeLink: 'statistics', icon: 'apps', label: 'Statistics' },
     { 
       routeLink: 'coupens', 
@@ -20,7 +64,24 @@ export class NavDataServiceService {
     },
     { routeLink: 'pages', icon: 'content_copy', label: 'Pages' },
     { routeLink: 'media', icon: 'play_circle', label: 'Media' },
-    { routeLink: 'settings', icon: 'settings', label: 'Settings' },
+    { 
+      routeLink: 'settings', 
+      icon: 'settings', 
+      label: 'Settings',
+      expanded: true,
+      items: [
+        {
+          routeLink: 'settings/profile',
+          icon: 'person',
+          label: 'Profile'
+        },
+        {
+          routeLink: 'settings/customize',
+          icon: 'settings_account_box',
+          label: 'Customize'
+        }
+      ]
+    },
   ];
 
   constructor() { }
